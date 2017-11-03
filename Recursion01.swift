7f44c93ed42d6e5a41eccc24bdce2c64581b609a
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-11-03 23:37:23 +0800

    Create Recursion01.swift

---
+++ Recursion01.swift
@@ -0,0 +1,21 @@
+'''
+use while to find key
+'''
+def look_for_key(main_box):
+  pile = main_box.make_a_pile_to_look_through()
+  while pile is not empty:
+    box = pile.grab_A_box()
+    for item in box:
+      if item.is_a_box():
+        pile.append(item)
+      elif item.is_a_key():
+        print "found the key!"
+'''
+use recursion to find key
+'''
+def look_for_key(box):
+  for item in box:
+    if item.is_a_box():
+      look_for_key(item)
+    elif item.is_a_key():
+      print "found the key"
