794f0cf5dcc275e29523122423d76b41493c5e33
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-11-11 09:54:48 +0800

    call stack/调用栈

--- Recursion03.py
+++ Recursion03.py
@@ -1,10 +1,10 @@
-def greet(name)
-  print "hello, " + name  + "!"
-  greet2(name)
-  print "getting  ready to say bye..."
-  bye()
-  
 def greet2(name):
-  print "how are you, " + name + "?"
-  def bye():
-    print "ok bye"
+    print( "how are you, " + name + "?")
+def bye():
+        print("ok bye!")
+def greet(name):
+    print("hello, " + name + "!")
+    greet2(name)
+    print("getting ready to say bye...")
+    bye()
+print(greet("Yingjie"))
