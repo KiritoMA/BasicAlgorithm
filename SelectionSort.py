27a90890024eabeb608f3d3a09317ff3abfe125e
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-11-14 11:09:23 +0800

    Update and rename SectionSort.py to SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,17 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1,len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def SelectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(SelectionSort([5,1,3,2,4]))
