011d515c3c58ea89467984289d2bfe2b5d0c1d5e
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-11-19 21:06:05 +0800

    Update Graph01.py

--- Graph01.py
+++ Graph01.py
@@ -9,18 +9,21 @@ graph["thom"] = []
 graph["jonny"] = []
 
 'deque is object in collections'
+from collections import deque
 
 def find_seller():
-    from collections import deque
     search_queue = deque()
     search_queue += graph["you"]
+    searched = []
     while search_queue:
         person = search_queue.popleft()
-        if person_is_seller(person):
-            print(person + " is a mango seller!")
-            return True
-        else:
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller!")
+                return True
+            else:
                 search_queue += graph[person]
+                searched.append(person)
     return False
 
 def person_is_seller(name):
