2f717500d1f6ff48ce3a22caf1d626655fbdb1a0
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-11-22 23:34:47 +0800

    Create DynamicProgramming01.py

---
+++ DynamicProgramming01.py
@@ -0,0 +1,11 @@
+'Longest Common Substring'
+if word_a[i] == word_b[j]:
+    cell[i][j] = cell[i-1][j-1] + 1
+else:
+    cell[i][j] = 0
+
+'longest common subsequence'
+if word_a[i] = word_b[j]:
+    cell[i][j] = cell[i-1][j-1] + 1
+else::
+    cell[i][j] = max(cell[i-1][j], cell[i][j-1])
