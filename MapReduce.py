8cc1a269a0797c71b5a545305bd6cd3f09a874fb
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-11-26 21:43:13 +0800

    Update MapReduce.py

--- MapReduce.py
+++ MapReduce.py
@@ -1,5 +1,10 @@
 arr1 = [1,2,3,4,5]
-#map get a iterator 
+#map get a iterator
 #in python3 list() is needed
 arr2 = list(map(lambda x: 2 * x, arr1))
 print(arr2)
+from functools  import reduce
+def f4(x,y):
+    return (x+y)
+arr3 = reduce(f4 ,arr1)
+print(arr3)
